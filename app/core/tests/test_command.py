0cf1aba8a955ec0695de52531b20c1e73be0e4cb
Rezasl99 <Rezasoltani78@gmail.com>
2024-10-17 19:00:27 +0330

    40

---
+++ app/core/tests/test_command.py
@@ -0,0 +1,19 @@
+
+#Test custom Django management commands.
+
+from unittest.mock import patch 
+from psycopg2 import OperationalError as Psycopg2Error
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.command.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+
+    def test_wait_for_db_ready(self, patched_check):
+        # waits for database if its ready
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(database=['default'])
